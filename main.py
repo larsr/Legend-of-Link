fab12f1745473746cb292f17c7c6ae3a62383d2e
Lars Rasmusson <Lars.Rasmusson@sics.se>
2011-06-02 21:51:40 +0200

    now picks a random userid, and just one http handler

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
@@ -10,41 +11,37 @@ gametexts = {}
 
 class MainPage(webapp.RequestHandler):
 
-  def get(self):
-    ip =  self.request.remote_addr
-    global games, gametexts
-    if not ip in games:
-        games[ip]=link.link()
-        gametexts[ip]=games[ip].next().replace('\n','<br>')
-        
-    template_values = {
-      'text':gametexts[ip]
-      }
-
-    path = os.path.join(os.path.dirname(__file__), 'index.html')
-    self.response.out.write(template.render(path, template_values))
-
-class Command(webapp.RequestHandler):
   def post(self):
-    ip =  self.request.remote_addr
-    global games,gametexts
-    cmd = self.request.get('content')[:80]
+      self.get()
 
-    if cmd == "restart":
-        games[ip] = link.link()
-        gametexts[ip] = games[name].next().replace('\n','<br>')
-    else:
-        try:
-            gametexts[ip] = games[ip].send(cmd).replace('\n','<br>').replace(" ","&nbsp;")
-            gametexts[ip] = gametexts[ip].replace("@@img@@","img src").replace("@@width@@"," width")
-        except StopIteration:
-            del games[ip]
-    self.redirect('/')
+  def get(self):
+    userid = self.request.get('userid')[:80]
+    if userid == None or userid == "":
+        userid = str(random.randint(1,10**10))
+        
+    global games, gametexts
 
-application = webapp.WSGIApplication(
-                                     [('/', MainPage),
-                                      ('/cmd', Command)],
-                                     debug=True)
+    if userid in games:
+        cmd = self.request.get('content')[:80]
+
+        if cmd == "restart":
+            del games[userid]
+        else:
+            try:
+                gametexts[userid] = games[userid].send(cmd).replace('\n','<br>').replace(" ","&nbsp;")
+                gametexts[userid] = gametexts[userid].replace("@@img@@","img src").replace("@@width@@"," width")
+            except StopIteration:
+                del games[userid]
+
+    if not userid in games:
+        games[userid]=link.link()
+        gametexts[userid]=games[userid].next().replace('\n','<br>')
+
+    template_path = os.path.join(os.path.dirname(__file__), 'index.html')
+    html = template.render(template_path, dict(text=gametexts[userid],userid=userid))
+    self.response.out.write(html)
+
+application = webapp.WSGIApplication([('/', MainPage)], debug=True)
 
 def main():
   run_wsgi_app(application)
